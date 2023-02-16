e39f884ac46645c422a9218834c48238e2949fe1
Oleg Samoilenko <olaggg-93so@yandex.ru>
2023-02-16 17:06:28 +0300

    middlewares/cors.js

--- frontend/src/utils/Api.jsx
+++ frontend/src/utils/Api.jsx
@@ -107,6 +107,7 @@ class Api {
 
 const api = new Api({
   baseUrl: "http://localhost:3000",
+  //baseUrl: "http://oleg021mesto.nomoredomains.club",
   headers: {
     //headers: this._headers,
     //authorization: `Bearer ${token},
